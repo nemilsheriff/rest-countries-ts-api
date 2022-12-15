e8914aad9112da223615de51ea4b79c061416c8f
Nemil Sheriff <nemil.sheriff@gmail.com>
2022-12-16 07:48:34 +0800

    all done

--- src/components/CountryDetails/CountryDetails.tsx
+++ src/components/CountryDetails/CountryDetails.tsx
@@ -37,7 +37,7 @@ export const CountryDetails = (props: CountryDetailsProps) => {
                         <svg xmlns="http://www.w3.org/2000/svg" height="24" width="24"><path d="m12 20-8-8 8-8 1.425 1.4-5.6 5.6H20v2H7.825l5.6 5.6Z" /></svg>
                         Back</a>
                   <div className="flag-and-details">
-                        <img src={props.country.flags.png} alt='flag'></img>
+                        <img className='flag-image' src={props.country.flags.png} alt='flag'></img>
                         <div className="text-details">
                               <h3>{props.country.name.official}</h3>
                               <p><strong>Native Name:</strong> {props.country.name.official}</p>
